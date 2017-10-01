db699afed2b2343bb7d5a707de0fedd8fcc42633
narko <6020peaks@gmail.com>
2017-10-01 21:47:16 +0200

    Added espresso test

---
+++ BakingApp/app/src/androidTest/java/com/a6020peaks/bakingapp/RecipeListTest.java
@@ -0,0 +1,33 @@
+package com.a6020peaks.bakingapp;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.a6020peaks.bakingapp.ui.list.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by narko on 01/10/17.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class RecipeListTest {
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule(MainActivity.class);
+
+    @Test
+    public void clickRecipe_OpensDetailsActivity() {
+        onView(withId(R.id.recipe_rv)).perform(actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.recipe_details_rv)).check(matches(isDisplayed()));
+    }
+}
