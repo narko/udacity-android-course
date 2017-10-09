1e23ca5620ef7b5ec0aed089c5bd68a93306652a
narko <antonio.roa.valverde@gmail.com>
2017-10-09 11:29:47 +0200

    Fixed bleed for new dataset

--- xyz-reader/XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ xyz-reader/XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -5,12 +5,15 @@ import java.net.URL;
 
 public class Config {
     public static final URL BASE_URL;
-    private static String TAG = Config.class.toString();
+    private static final String TAG = Config.class.toString();
+    private static final String URL1 = "https://go.udacity.com/xyz-reader-json";
+    private static final String URL2 = "https://raw.githubusercontent.com/TNTest/xyzreader/master/data.json";
+
 
     static {
         URL url = null;
         try {
-            url = new URL("https://go.udacity.com/xyz-reader-json");
+            url = new URL(URL2);
         } catch (MalformedURLException ignored) {
             // throw a real error
             throw new RuntimeException("Error creating URL");
