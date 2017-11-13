96f39db1a703f177dadbe9d63018c82bb0361b03
narko <antonio.roa.valverde@gmail.com>
2017-11-13 18:58:28 +0100

    Fixed issue with DB test running on UI thread

--- BakingApp/app/src/androidTest/java/com/a6020peaks/bakingapp/RecipeRepositoryTest.java
+++ BakingApp/app/src/androidTest/java/com/a6020peaks/bakingapp/RecipeRepositoryTest.java
@@ -1,13 +1,12 @@
 package com.a6020peaks.bakingapp;
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule;
+import android.os.AsyncTask;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.a6020peaks.bakingapp.data.RecipeRepository;
-import com.a6020peaks.bakingapp.data.database.IngredientEntry;
 import com.a6020peaks.bakingapp.data.database.RecipeEntry;
-import com.a6020peaks.bakingapp.data.database.RecipeWithIngredients;
 import com.a6020peaks.bakingapp.utils.InjectorUtils;
 
 import org.junit.Before;
@@ -17,8 +16,6 @@ import org.junit.runner.RunWith;
 
 import java.util.List;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
 
 /**
@@ -37,27 +34,25 @@ public class RecipeRepositoryTest {
 
     @Test
     public void getRecipeDetailsTest() throws InterruptedException {
-        // TODO this test is incomplete. There are issues happening when returning RecipeDetails
-        // since most of the data is empty.
+        new AsyncTask<Void, Void, List<RecipeEntry>>() {
 
-        List<RecipeEntry> recipes = LiveDataTestUtil.getValue(mRepository.getRecipes());
-        assertTrue(recipes != null && recipes.size() > 0);
-        RecipeEntry recipe = recipes.get(0);
-        System.out.println(recipe.getId());
-//        RecipeDetails recipeDetails = LiveDataTestUtil.getValue(mRepository.getRecipeDetails(recipe.getId()));
-//        assertTrue(recipeDetails != null);
-//        List<IngredientEntry> ingredients = recipeDetails.getRecipeWithIngredients();
-//        assertTrue(ingredients != null);
-//        IngredientEntry ingredient = ingredients.get(0);
-//        assertTrue(ingredient.getRecipeId() == recipe.getId());
+            @Override
+            protected List<RecipeEntry> doInBackground(Void... voids) {
+                List<RecipeEntry> recipes = null;
+                try {
+                    recipes = LiveDataTestUtil.getValue(mRepository.getRecipes());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return recipes;
+            }
 
-        RecipeWithIngredients recipeWithIngredients = mRepository.getRecipeWithIngredients(recipe.getId());
-        assertTrue(recipeWithIngredients != null);
-        System.out.println(recipeWithIngredients.getRecipe().getName());
-        List<IngredientEntry> ingredients = recipeWithIngredients.getIngredients();
-        assertNotNull(ingredients);
-        IngredientEntry ingredientEntry = ingredients.get(0);
-        System.out.println(ingredientEntry.getDescription());
-        assertEquals(recipe.getId(), ingredientEntry.getRecipeId());
+            @Override
+            protected void onPostExecute(List<RecipeEntry> recipes) {
+                assertTrue(recipes != null && recipes.size() > 0);
+                RecipeEntry recipe = recipes.get(0);
+                System.out.println(recipe.getId());
+            }
+        }.execute();
     }
 }
